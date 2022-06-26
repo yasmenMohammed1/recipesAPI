949a8073edf0250b15cee959342e0b4b133e7596
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2022-06-26 03:49:42 +0200

    update

--- src/controllers/guesr.controller.ts
+++ src/controllers/guesr.controller.ts
@@ -3,12 +3,18 @@ import { Ipservice } from '../services/guest.service';
 import { BaseController } from './base.controller';
 import { NextFunction, Request, Response } from 'express';
 import mongoose, { model } from 'mongoose';
-
+import { exec } from 'child_process';
+import http from 'http';
 export class GuestIp extends BaseController<Ips> {
   _serviceObj: Ipservice = new Ipservice();
   async getIp(req: Request, res: Response, next: NextFunction) {
-    const ip = req.socket.localAddress;
+    let ip: any;
     try {
+      exec('curl ip-adresim.app', function (error: any, stdout: any, stderr: any) {
+        if (error) return;
+        console.log('your ip is :' + stdout);
+        ip = stdout;
+      });
       new Promise(async (res, rej) => {
         await model('ips')
           .create({
